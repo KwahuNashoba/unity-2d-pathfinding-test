801cae694ba71f5daef0ecc7e38338ca6078575d
Данило Хаџи-Пешић <danilo.hadzi.pesic@gmail.com>
2020-07-19 14:54:36 +0200

    Created Options pop-up that loads or stores options directly from/to options scriptable object (which is not persistent)

---
+++ Assets/Scripts/UI/OptionsPopupController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsPopupController : MonoBehaviour
+{
+    [SerializeField] private OptionSettings options;
+
+    [SerializeField] private InputField gridSizeInput;
+    [SerializeField] private InputField totalObstaclesInput;
+    [SerializeField] private XYInput startPositionInput;
+    [SerializeField] private XYInput endPositionInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PopulateFieldsFromOptions();
+        RegisterCallbacks();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void PopulateFieldsFromOptions()
+    {
+        gridSizeInput.text = options.gridSize.ToString();
+        totalObstaclesInput.text = options.totalObstacles.ToString();
+        startPositionInput.SetValue(options.startPosition);
+        endPositionInput.SetValue(options.endPosition);
+    }
+
+    private void RegisterCallbacks()
+    {
+        gridSizeInput.onValueChanged.AddListener(OnGridSizeChanged);
+        totalObstaclesInput.onValueChanged.AddListener(OnTotalObstaclesChanged);
+        startPositionInput.onValueChanged = OnStartPositionChanged;
+        startPositionInput.onValueChanged = OnEndPositionChanged;
+    }
+
+    public void OnGridSizeChanged(string newSize)
+    {
+        int parsedSize;
+        if(int.TryParse(newSize, out parsedSize))
+        {
+            options.gridSize = parsedSize;
+        }
+    }
+
+    public void OnTotalObstaclesChanged(string newTotal)
+    {
+        int parsedTotal;
+        if(int.TryParse(newTotal, out parsedTotal))
+        {
+            options.totalObstacles = parsedTotal;
+        }
+    }
+    public void OnStartPositionChanged(Vector2Int newValue)
+    {
+        options.startPosition = newValue;
+    }
+
+    public void OnEndPositionChanged(Vector2Int newValue)
+    {
+        options.endPosition = newValue;
+    }
+}
---
+++ Assets/Scripts/UI/XYInput.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class XYInput : MonoBehaviour
+{
+    [SerializeField] private InputField xInput;
+    [SerializeField] private InputField yInput;
+
+    public UnityAction<Vector2Int> onValueChanged;
+
+    void Start()
+    {
+        RegisterCallbacks();
+    }
+    
+    public void SetValue(Vector2Int newValue)
+    {
+        xInput.text = newValue.x.ToString();
+        yInput.text = newValue.y.ToString();
+    }
+
+    private void RegisterCallbacks()
+    {
+        xInput.onValueChanged.AddListener(OnValuesChanged);
+        yInput.onValueChanged.AddListener(OnValuesChanged);
+    }
+
+    private void OnValuesChanged(string value)
+    {
+        if(onValueChanged != null)
+        {
+            Vector2Int newValue = new Vector2Int();
+            int x, y;
+            if(int.TryParse(xInput.text, out x))
+            {
+                newValue.x = x;
+            }
+            if(int.TryParse(yInput.text, out y))
+            {
+                newValue.y = y;
+            }
+
+            onValueChanged(newValue);
+        }
+    }
+}
